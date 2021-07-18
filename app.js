dd1eeaf98c93de3c3b765b725e8548b79c4a55a3
dionjwa1 <dionjwa@gmail.com>
2021-07-18 11:41:07 -0700

    depends

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 'use strict'
 import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 let dion = 'Hey Yo D!';
 console.log(dion);
 
-
+ReactDOM.render(
 <Container fluid>
   <Row>
-    <Col>1 of 1</Col>
+    <Col>This is the start of something great!</Col>
   </Row>
-</Container>
+</Container>,
 
-
+document.getElementById("root")
+);
